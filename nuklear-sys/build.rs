8ec74778bcd0d929073cc6db7527f11d411b66c5
Serhii Plyhun <snuk188@gmail.com>
2018-01-16 14:09:49 +0100

    Improve -sys package build. Closes #11.

--- nuklear-sys/build.rs
+++ nuklear-sys/build.rs
@@ -1,5 +1,17 @@
-extern crate gcc;
+extern crate cc;
+
+use std::path::Path;
+use std::process::Command;
 
 fn main() {
-    gcc::compile_library("libnuklear.a", &["nuklear-c/bind.c"]);
+    if !Path::new("nuklear-c/nuklear/.git").exists() {
+        Command::new("git")
+            .args(&["submodule", "update", "--init", "--recursive"])
+            .status()
+            .unwrap();
+    }
+    cc::Build::new()
+        .file("nuklear-c/bind.c")
+        .compile("libnuklear");
+    //gcc::compile_library("libnuklear.a", &["nuklear-c/bind.c"]);
 }
